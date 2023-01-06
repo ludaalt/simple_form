6284e96bcdaec1c9c58267b90eaa8553b1deacce
Людмила Алтухова <luda.tamb@mail.ru>
2023-01-06 23:52:14 +0300

    feat: add form validation

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,15 @@
+import { useState, useEffect } from "react";
 import styled from "styled-components";
+import Typography from "@mui/material/Typography";
 
 import FormInput from "./FormInput";
 import FormButton from "./FormButton";
 import FormTextArea from "./FormTextArea";
+import { isUserNameValid } from "../helpers/isUserNameValid";
+import { isUserTelephoneValid } from "../helpers/isUserTelephoneValid";
+import { formatUserTelephone } from "../helpers/formatUserTelephone";
+
+import { MAX_PASSWORD_LENGTH } from "../const/passwordConst";
 
 const StyledForm = styled.form`
   min-width: 400px;
@@ -37,17 +44,73 @@ const StyledForm = styled.form`
 `;
 
 const Form = () => {
+  const [error, setError] = useState("");
+  const [userData, setUserData] = useState({ name: "", telephone: "" });
+
+  useEffect(() => {
+    if (userData.telephone.length === MAX_PASSWORD_LENGTH) {
+      setUserData((state) => ({
+        ...state,
+        telephone: formatUserTelephone(userData.telephone),
+      }));
+    }
+  }, [userData.telephone]);
+
+  const submitForm = (e) => {
+    e.preventDefault();
+
+    if (!isUserNameValid(userData.name)) {
+      setError("Введите корректное имя");
+      return;
+    }
+
+    if (!isUserTelephoneValid(userData.telephone)) {
+      setError("Введите корректный номер телефона");
+      return;
+    }
+
+    try {
+      alert(`Данные успешно отправлены`);
+    } catch (e) {
+      setError(e);
+    } finally {
+      e.target.reset();
+      setError("");
+    }
+  };
+
   return (
-    <StyledForm>
+    <StyledForm onSubmit={submitForm}>
+      {error && (
+        <Typography variant="caption" color="error">
+          {error}
+        </Typography>
+      )}
+
       <FormInput
+        error={!!error}
+        helperText={error}
         type="text"
         placeholder="Иван Иванов"
         label="Введите своё имя"
+        value={userData.name}
+        changeHandler={(e) =>
+          setUserData((state) => ({ ...state, name: e.target.value }))
+        }
       />
+
       <FormInput
         type="tel"
         placeholder="+79999999999"
         label="Введите телефон"
+        value={userData.telephone}
+        changeHandler={(e) => {
+          setUserData((state) => ({ ...state, telephone: e.target.value }));
+        }}
+        isError={
+          userData.telephone.length > 0 &&
+          !isUserTelephoneValid(userData.telephone)
+        }
       />
 
       <FormTextArea placeholder="Введите сообщение..." />
